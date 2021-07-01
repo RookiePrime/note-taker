201e0e554147b9e44ea7848f886e9658ca8c1e53
Kiefer Levine <kiefer_8@hotmail.com>
2021-07-01 14:33:50 -0400

    Now takes DELETE route, has basic HTML

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const { createNote, removeNote } = require('./lib/notes');
+const { createNote, removeNote, findById } = require('./lib/notes');
 
 const { notes } = require('./db/db.json');
 
@@ -11,8 +11,27 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// HTML Routes
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+// API Routes
 app.get('/api/notes', (req, res) => {
     res.json(notes);
+});
+
+app.get('/api/notes/:id', (req, res) => {
+    const result = findById(req.params.id, notes);
+    if (result) {
+        res.json(result);
+    } else {
+        res.sendStatus(404);
+    }
 });
 
 app.post('/api/notes', (req, res) => {
@@ -24,8 +43,8 @@ app.post('/api/notes', (req, res) => {
 });
 
 app.delete('/api/notes/:id', (req, res) => {
-    const note = req.params.id;
-    removeNote(note, notes);
+    const noteId = req.params.id;
+    removeNote(noteId, notes);
     res.json({
         message: 'Note removed from notes!'
     });
